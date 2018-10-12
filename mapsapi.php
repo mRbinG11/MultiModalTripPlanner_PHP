18beb866bb958be750561511aa1527769db92516
Sujith Bhat <43529659+mRbinG11@users.noreply.github.com>
2018-10-12 18:09:46 +0530

    Add files via upload

---
+++ mapsapi.php
@@ -0,0 +1,90 @@
+<html>
+  <head>
+    <style>      
+    #placemap 
+      {
+        height: 50%;
+        width: 30%;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="placemap"></div>
+<?php
+function googleapi($url) 
+{
+	$options=array(
+		CURLOPT_RETURNTRANSFER => true,			//returns webpage
+		CURLOPT_HEADER => false,				//no headers to return
+		CURLOPT_FOLLOWLOCATION => true,			//follow redirects
+		CURLOPT_MAXREDIRS => 10,				//stop after 10 redirects
+		CURLOPT_ENCODING => "",					//handle compressed
+		CURLOPT_USERAGENT => "test",			//name of client
+		CURLOPT_AUTOREFERER => true,			//set referrer on redirect
+		CURLOPT_CONNECTTIMEOUT => 120,			//time out on connect
+		CURLOPT_TIMEOUT => 120,					//time out on response
+	);
+
+	$ch=curl_init($url);
+	curl_setopt_array($ch, $options);
+	$content=curl_exec($ch);
+	curl_close($ch);
+	return $content;
+
+}
+
+	$source=$_POST['bussource'];
+	$dest=$_POST['busdest'];
+	$destname=urlencode($dest);
+	$sourcename=urlencode($source);
+	//$response_directions = googleapi("https://maps.googleapis.com/maps/api/directions/json?origin=".$sourcename."&destination=".$destname."&key=AIzaSyDnfJA1FX86cFbGxLE9-BnseJXAZ41b8Ek");
+
+	
+
+	$resdir=array();
+	$resdir=json_decode($response_directions,true);
+	//print_r($resdir);
+	$temp=$resdir['routes'];
+	$t=$temp[0];
+	$a=$t['bounds'];
+	$b=$a['northeast'];
+	$lat=$b['lat'];
+	$long=$b['lng'];
+	echo $lat;
+	echo $long;
+
+	//$response_places=googleapi("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$lat.",".$long."&radius=1500&type=bus_station&keyword=cruise&key=AIzaSyDnfJA1FX86cFbGxLE9-BnseJXAZ41b8Ek");
+	$resplace=array();
+	$resplace=json_decode($response_places,true);
+	print_r($resplace);
+
+
+?>
+<script type="text/javascript">
+	
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('placemap'), {
+          zoom: 3,
+          center: {lat: , lng: },
+          mapTypeId: 'terrain'
+        });
+
+        var points = [
+          {lat: , lng: },
+          {lat: , lng: }
+        ];
+        var pathway = new google.maps.Polyline({
+          path: points,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+        pathway.setMap(map);
+      }
+</script>
+<script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDnfJA1FX86cFbGxLE9-BnseJXAZ41b8Ek&callback=initMap">
+    </script>
+
